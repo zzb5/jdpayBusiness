55be54b2bb40baf81fa3cfa5f4e2896fcdc6f198
zhaozibo6 <zhaozibo6@jd.com>
2024-06-05 21:22:24 +0800

    init

--- src/main/java/com/jd/bcp/jdpayBusiness/link/baitiaoQuickLink/Script1.java
+++ src/main/java/com/jd/bcp/jdpayBusiness/link/baitiaoQuickLink/Script1.java
@@ -1,18 +1,29 @@
 package com.jd.bcp.jdpayBusiness.link.baitiaoQuickLink;
 
+import com.jd.bcp.common.annotation.NodeScript;
 import com.jd.bcp.common.utils.GsonUtil;
+import com.jd.bcp.common.vo.CustomAction;
+import com.jd.bcp.common.vo.ExecuteContext;
+import com.jd.bcp.common.service.ScriptExecutor;
 import com.jd.bcp.jdpayBusiness.common.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Script1 {
-    public Map<String, String> execute(String data) {
-        User user = GsonUtil.fromJson(data, User.class);
-        System.out.println(user.getName());
-        System.out.println(user.getAge());
-        Map<String, String> result = new HashMap<>();
-        result.put(user.getName(), user.getAge());
-        return result;
+@NodeScript(nodeId = "preparePay")
+public class Script1 implements ScriptExecutor {
+
+    @Override
+    public List<CustomAction<?>> execute(ExecuteContext executeContext) {
+        List<CustomAction<?>> customActions = new ArrayList<>();
+        CustomAction<User> userCustomAction = new CustomAction<>();
+        customActions.add(userCustomAction);
+        User user = new User();
+        user.setName("Script1");
+        userCustomAction.setCustomActionInfo(user);
+        userCustomAction.setCustomActionType("user");
+        return customActions;
     }
 }
--- src/main/java/com/jd/bcp/jdpayBusiness/link/commonLink/Script3.java
+++ src/main/java/com/jd/bcp/jdpayBusiness/link/commonLink/Script3.java
@@ -1,17 +1,29 @@
 package com.jd.bcp.jdpayBusiness.link.commonLink;
 
+import com.jd.bcp.common.annotation.NodeScript;
+import com.jd.bcp.common.service.ScriptExecutor;
 import com.jd.bcp.common.utils.GsonUtil;
+import com.jd.bcp.common.vo.CustomAction;
+import com.jd.bcp.common.vo.ExecuteContext;
 import com.jd.bcp.jdpayBusiness.common.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Script3 {
-    public Map<String, String> execute(String data) {
-        User user = GsonUtil.fromJson(data, User.class);
-        Map<String, String> result = new HashMap<>();
-        result.put(user.getName(), user.getAge());
-        result.put(user.getAge(), user.getName());
-        return result;
+@NodeScript(nodeId = "preparePay")
+public class Script3 implements ScriptExecutor {
+
+    @Override
+    public List<CustomAction<?>> execute(ExecuteContext executeContext) {
+        List<CustomAction<?>> customActions = new ArrayList<>();
+        CustomAction<User> userCustomAction = new CustomAction<>();
+        customActions.add(userCustomAction);
+        User user = new User();
+        user.setName("Script3");
+        userCustomAction.setCustomActionInfo(user);
+        userCustomAction.setCustomActionType("user");
+        return customActions;
     }
 }
