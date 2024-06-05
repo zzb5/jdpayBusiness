54c4ea3d61b8beb412aa56a85f79e7537624b978
zhaozibo6 <zhaozibo6@jd.com>
2024-06-05 21:31:19 +0800

    init

--- src/main/java/com/jd/bcp/jdpayBusiness/common/User.java
+++ src/main/java/com/jd/bcp/jdpayBusiness/common/User.java
@@ -20,4 +20,9 @@ public class User {
     public void setAge(String age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\'' + ", age='" + age + '\'' + '}';
+    }
 }
