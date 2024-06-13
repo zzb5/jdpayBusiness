309f66450a3477620a75cb155feb9743610bdcaf
zhaozibo6 <zhaozibo6@jd.com>
2024-06-13 16:41:56 +0800

    init

--- src/main/java/com/jd/bcp/jdpayBusiness/link/commonLink/jdpaySdkPay.java
+++ src/main/java/com/jd/bcp/jdpayBusiness/link/commonLink/jdpaySdkPay.java
@@ -17,7 +17,7 @@ public class jdpaySdkPay implements JavaExecutor {
     public List<CustomAction<?>> execute(ExecuteContext executeContext) {
         List<CustomAction<?>> customActions = new ArrayList<>();
         CustomAction<ReportAlertCustomVo> reportAlert = CustomManager.build(CustomActionTypeEnum.REPORT_ALERT);
-        reportAlert.setCustomActionInfo(new ReportAlertCustomVo("pay接口异常, 第1三次修改"));
+        reportAlert.setCustomActionInfo(new ReportAlertCustomVo("pay接口异常, 第三次修改"));
         customActions.add(reportAlert);
         return customActions;
     }
